92afb7cf73ebb8e3934df16df46a5c010d48323d
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2020-11-28 13:54:21 +0200

    check if a database is dropped

--- tests/ThrowawayDb/Program.cs
+++ tests/ThrowawayDb/Program.cs
@@ -1,44 +1,78 @@
 ﻿using System;
 using System.Data.SqlClient;
+using FluentAssertions;
 using ThrowawayDb;
 
 namespace tests
 {
 	internal class Program
-    {
-		private const string ConnectionString = "localhost\\SQLEXPRESS";
+	{
+		private const string LocalInstanceName = "localhost\\SQLEXPRESS";
+
+		private static ThrowawayDatabase CreateFixture() =>
+			ThrowawayDatabase.FromLocalInstance(LocalInstanceName);
 
 		private static void Main(string[] args)
-        {
-            var arguments = string.Join(", ", args);
-            Console.WriteLine($"Arguments [{arguments}]");
+		{
+			var arguments = string.Join(", ", args);
+			Console.WriteLine($"Arguments [{arguments}]");
 
-            var testCases = new Action<ThrowawayDatabase>[]
-            {
-	            TestDatabaseCreation
-            };
+			var testCases = new Action[]
+			{
+				CreateDatabase
+			};
 
-            using var fixture = ThrowawayDatabase.FromLocalInstance(ConnectionString);
+			for (var i = 0; i < testCases.Length; i++)
+			{
+				Console.WriteLine("Running test #{0}: {1}", i + 1, testCases[i].Method.Name);
+				testCases[i]();
 
-            for (var i = 0; i < testCases.Length; i++)
-            {
-	            Console.WriteLine("Running test #{0}: {1}", i + 1, testCases[i].Method.Name);
-	            testCases[i](fixture);
-            }
-        }
+				Console.WriteLine();
+			}
+		}
 
-	    private static void TestDatabaseCreation(ThrowawayDatabase fixture)
-	    {
-		    Console.WriteLine($"Created database {fixture.Name}");
-		    
-		    // Apply migrations / seed data if necessary
-		    // execute code against this newly generated database
-		    using var connection = new SqlConnection(fixture.ConnectionString);
-		    connection.Open();
-		    
-		    using var cmd = new SqlCommand("SELECT 1", connection);
-		    var result = Convert.ToInt32(cmd.ExecuteScalar());
-		    Console.WriteLine(result); // prints 1
-	    }
-    }
+		private static void CreateDatabase()
+		{
+			string databaseName;
+
+			using (var fixture = CreateFixture())
+			{
+				databaseName = fixture.Name;
+				Console.WriteLine($"Created database {fixture.Name}");
+
+				CheckDatabaseExists(databaseName)
+					.Should()
+					.BeTrue();
+
+				// Apply migrations / seed data if necessary
+				// execute code against this newly generated database
+				using var connection = new SqlConnection(fixture.ConnectionString);
+				connection.Open();
+
+				using var cmd = new SqlCommand("SELECT 1", connection);
+				var result = Convert.ToInt32(cmd.ExecuteScalar());
+				Console.WriteLine(result); // prints 1
+
+				result
+					.Should()
+					.Be(1);
+			}
+
+			CheckDatabaseExists(databaseName)
+				.Should()
+				.BeFalse();
+
+			static bool CheckDatabaseExists(string name)
+			{
+				using var connection = new SqlConnection($"Data Source={LocalInstanceName};Initial Catalog=master;Integrated Security=True;");
+				connection.Open();
+
+				using var cmd = new SqlCommand($"SELECT CASE WHEN DB_ID(@{nameof(name)}) IS NULL THEN 0 ELSE 1 END", connection);
+				cmd.Parameters.AddWithValue(nameof(name), name);
+
+				var result = cmd.ExecuteScalar();
+				return Convert.ToInt32(result) == 1;
+			}
+		}
+	}
 }
