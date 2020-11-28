d4e3ea20fb551190870935137314236f40f69192
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2020-11-28 13:37:03 +0200

    split tests into functions

--- tests/ThrowawayDb/Program.cs
+++ tests/ThrowawayDb/Program.cs
@@ -4,28 +4,41 @@ using ThrowawayDb;
 
 namespace tests
 {
-    class Program
+	internal class Program
     {
-        static void Main(string[] args)
+		private const string ConnectionString = "localhost\\SQLEXPRESS";
+
+		private static void Main(string[] args)
         {
-            var arguments = String.Join(", ", args);
+            var arguments = string.Join(", ", args);
             Console.WriteLine($"Arguments [{arguments}]");
 
-            using (var database = ThrowawayDatabase.FromLocalInstance("localhost\\SQLEXPRESS"))
+            var testCases = new Action<ThrowawayDatabase>[]
             {
-                Console.WriteLine($"Created database {database.Name}");
-                // Apply migrations / seed data if necessary
-                // execute code against this newly generated database
-                using (var connection = new SqlConnection(database.ConnectionString))
-                {
-                    connection.Open();
-                    using (var cmd = new SqlCommand("SELECT 1", connection))
-                    {
-                        var result = Convert.ToInt32(cmd.ExecuteScalar());
-                        Console.WriteLine(result); // prints 1
-                    }
-                }
+	            TestDatabaseCreation
+            };
+
+            using var fixture = ThrowawayDatabase.FromLocalInstance(ConnectionString);
+
+            for (var i = 0; i < testCases.Length; i++)
+            {
+	            Console.WriteLine("Running test #{0}: {1}", i + 1, testCases[i].Method.Name);
+	            testCases[i](fixture);
             }
         }
+
+	    private static void TestDatabaseCreation(ThrowawayDatabase fixture)
+	    {
+		    Console.WriteLine($"Created database {fixture.Name}");
+		    
+		    // Apply migrations / seed data if necessary
+		    // execute code against this newly generated database
+		    using var connection = new SqlConnection(fixture.ConnectionString);
+		    connection.Open();
+		    
+		    using var cmd = new SqlCommand("SELECT 1", connection);
+		    var result = Convert.ToInt32(cmd.ExecuteScalar());
+		    Console.WriteLine(result); // prints 1
+	    }
     }
 }
