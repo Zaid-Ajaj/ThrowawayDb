ceb527d936490c0641b8d215e730b3ec6ce4795b
Jesper Bo Rasmussen <jbr@oxygen.dk>
2021-02-23 21:00:11 +0100

    Add SnapshotScope and extension method on ThrowawayDatabase

---
+++ src/ThrowawayDb/SnapshotScope.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ThrowawayDb
+{
+    public class SnapshotScope : IDisposable
+    {
+        private ThrowawayDatabase _db;
+
+        public SnapshotScope(ThrowawayDatabase db)
+        {
+            _db = db;
+            _db.CreateSnapshot();
+        }
+
+        public void Dispose()
+        {
+            _db?.RestoreSnapshot();
+            _db = null;
+        }
+    }
+}
