300fbf7af98ec256b44c1a7db6e5ba55304a5011
Zaid <zaid.naom@gmail.com>
2021-03-23 09:06:16 +0100

    ThrowawayDb.MySql v1.0 thanks to @MihailsKuzmins (#15)

--- build/Build.cs
+++ build/Build.cs
@@ -16,10 +16,12 @@ using static Nuke.Common.Tooling.ProcessTasks;
 class Build : NukeBuild
 {
     static AbsolutePath Source => RootDirectory / "src";
-    static AbsolutePath ThrowawayDb => Source / "ThrowawayDb"; 
+    static AbsolutePath ThrowawayDb => Source / "ThrowawayDb";
     static AbsolutePath ThrowawayDbPostgres => Source / "ThrowawayDb.Postgres";
-    static AbsolutePath ThrowawayDbTests => RootDirectory / "tests" / "ThrowawayDb";
+    static AbsolutePath ThrowawayDbMySql => Source / "ThrowawayDb.MySql";
+    static AbsolutePath ThrowawayDbTests => RootDirectory / "tests" / "ThrowawayDb.SqlServer";
     static AbsolutePath ThrowawayDbPostgresTests => RootDirectory / "tests" / "ThrowawayDb.Postgres";
+    static AbsolutePath ThrowawayDbMySqlTests => RootDirectory / "tests" / "ThrowawayDb.MySql";
     static AbsolutePath PublishDir => RootDirectory / "publish";
     static readonly string DOTNET = "dotnet";
     public static int Main () => Execute<Build>(x => x.Test);
@@ -27,14 +29,14 @@ class Build : NukeBuild
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-    Target Clean => task => 
-      task  
+    Target Clean => task =>
+      task
         .Executes(() =>
         {
             var directories = new List<string> {
                 ThrowawayDb / "bin",
-                ThrowawayDb / "obj", 
-                ThrowawayDbPostgres / "bin", 
+                ThrowawayDb / "obj",
+                ThrowawayDbPostgres / "bin",
                 ThrowawayDbPostgres / "obj",
                 ThrowawayDbTests / "bin",
                 ThrowawayDbTests / "obj",
@@ -43,7 +45,7 @@ class Build : NukeBuild
                 PublishDir
             };
 
-            foreach(var dir in directories) 
+            foreach(var dir in directories)
             {
                 DeleteDirectory(dir);
             }
@@ -57,13 +59,17 @@ class Build : NukeBuild
             StartProcess(DOTNET, "build", ThrowawayDb).AssertZeroExitCode();
             StartProcess(DOTNET, "restore --no-cache", ThrowawayDbPostgres).AssertZeroExitCode();
             StartProcess(DOTNET, "build", ThrowawayDbPostgres).AssertZeroExitCode();
+            StartProcess(DOTNET, "restore --no-cache", ThrowawayDbMySql).AssertZeroExitCode();
+            StartProcess(DOTNET, "build", ThrowawayDbMySql).AssertZeroExitCode();
             StartProcess(DOTNET, "restore --no-cache", ThrowawayDbTests).AssertZeroExitCode();
             StartProcess(DOTNET, "build", ThrowawayDbTests).AssertZeroExitCode();
             StartProcess(DOTNET, "restore --no-cache", ThrowawayDbPostgresTests).AssertZeroExitCode();
             StartProcess(DOTNET, "build", ThrowawayDbPostgresTests).AssertZeroExitCode();
+            StartProcess(DOTNET, "restore --no-cache", ThrowawayDbMySqlTests).AssertZeroExitCode();
+            StartProcess(DOTNET, "build", ThrowawayDbMySqlTests).AssertZeroExitCode();
         });
 
-    Target Test => task => 
+    Target Test => task =>
       task
         .DependsOn(Compile)
         .Executes(() =>
@@ -78,9 +84,9 @@ class Build : NukeBuild
         });
 
     Target PackThrowawayDb => task =>
-      task 
+      task
         .DependsOn(Compile)
-        .Executes(() => 
+        .Executes(() =>
         {
             var packCmd = $"pack -c Release -o {PublishDir}";
             StartProcess(DOTNET, packCmd, ThrowawayDb).AssertZeroExitCode();
@@ -88,8 +94,8 @@ class Build : NukeBuild
 
     Target PublishThrowawayDb => task =>
       task
-        .DependsOn(PackThrowawayDb) 
-        .Executes(() => 
+        .DependsOn(PackThrowawayDb)
+        .Executes(() =>
         {
             var nugetFile = Directory.GetFiles(PublishDir).FirstOrDefault() ?? "";
             if (!nugetFile.EndsWith(".nupkg"))
@@ -112,18 +118,27 @@ class Build : NukeBuild
         });
 
     Target PackThrowawayDbPostgres => task =>
-      task 
+      task
         .DependsOn(Compile)
-        .Executes(() => 
+        .Executes(() =>
         {
             var packCmd = $"pack -c Release -o {PublishDir}";
             StartProcess(DOTNET, packCmd, ThrowawayDbPostgres).AssertZeroExitCode();
         });
 
+    Target PackThrowawayDbMySql => task =>
+      task
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            var packCmd = $"pack -c Release -o {PublishDir}";
+            StartProcess(DOTNET, packCmd, ThrowawayDbMySql).AssertZeroExitCode();
+        });
+
     Target PublishThrowawayDbPostgres => task =>
       task
-        .DependsOn(PackThrowawayDbPostgres) 
-        .Executes(() => 
+        .DependsOn(PackThrowawayDbPostgres)
+        .Executes(() =>
         {
             var nugetFile = Directory.GetFiles(PublishDir).FirstOrDefault() ?? "";
             if (!nugetFile.EndsWith(".nupkg"))
@@ -143,5 +158,30 @@ class Build : NukeBuild
 
             var nugetFileName = new FileInfo(nugetFile).Name;
             StartProcess(DOTNET, $"nuget push {nugetFileName} -s https://api.nuget.org/v3/index.json -k {nugetApiKey}", PublishDir).AssertZeroExitCode();
-        });        
+        });
+
+    Target PublishThrowawayDbMySql => task =>
+      task
+        .DependsOn(PackThrowawayDbMySql)
+        .Executes(() =>
+        {
+            var nugetFile = Directory.GetFiles(PublishDir).FirstOrDefault() ?? "";
+            if (!nugetFile.EndsWith(".nupkg"))
+            {
+                Logger.Error("No nuget package found");
+                System.Diagnostics.Process.GetCurrentProcess().Kill();
+            }
+
+            Logger.Info($"About to publish nuget package: {nugetFile}");
+            var nugetApiKey = EnsureVariable("NUGET_KEY") ?? "";
+
+            if (string.IsNullOrWhiteSpace(nugetApiKey))
+            {
+                Logger.Error("Nuget API Key was not setup on your local machine, missing environment variable NUGET_KEY");
+                System.Diagnostics.Process.GetCurrentProcess().Kill();
+            }
+
+            var nugetFileName = new FileInfo(nugetFile).Name;
+            StartProcess(DOTNET, $"nuget push {nugetFileName} -s https://api.nuget.org/v3/index.json -k {nugetApiKey}", PublishDir).AssertZeroExitCode();
+        });
 }
